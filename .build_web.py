ed395a2ea6c9985852bca9ae8be09593bbebb740
nullstalgia <nullstalgia@gmail.com>
2020-03-28 15:10:09 -0700

    Don't rebuild web assets if nothing has changed. (#16)

--- .build_web.py
+++ .build_web.py
@@ -1,11 +1,11 @@
+#!/usr/bin/python3
 from shutil import copyfile
 from subprocess import check_output, CalledProcessError
 import sys
 import os
 import platform
 import subprocess
-
-Import("env")
+from pathlib import Path
 
 def is_tool(name):
     cmd = "where" if platform.system() == "Windows" else "which"
@@ -31,6 +31,7 @@ def build_web():
                 os.mkdir("../dist")
 
             copyfile("build/web_assets.h", "../dist/web_assets.h")
+                
         except BaseException as e:
             raise BaseException("Error building web assets: " + e)
         finally:
@@ -46,4 +47,33 @@ def build_web():
 
         raise BaseException("Could not build web assets.  Please install nodejs.")
 
-build_web()
+def is_ignored(filename):
+    ignored_paths = ["build/", ".cache/", "dist/"]
+    if os.path.basename(filename).startswith(".") or os.path.isdir(filename):
+        return True
+    for check_path in ignored_paths:
+        if check_path in filename:
+            return True
+    return False
+
+def should_build():
+    asset_path = "dist/web_assets.h"
+    directory = "web/"
+    if not os.path.exists(asset_path):
+        return True
+    else:
+        timestamp = os.stat(asset_path).st_mtime
+
+        for file in Path(directory).glob('**/*'):
+            filename = str(file)
+            if is_ignored(filename):
+                continue
+            if os.stat(filename).st_mtime > timestamp:
+                print(filename+" was modified.")
+                return True
+        return False
+
+if should_build():
+    build_web()
+else:
+    print("No need to rebuild web assets.")
