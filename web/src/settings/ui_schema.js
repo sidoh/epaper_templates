7af584726c37c888ff297d71d9cdc23c840c94eb
Chris Mullins <chris@sidoh.org>
2020-03-29 22:18:08 -0700

    Add SPI bus setting definition to the UI

--- web/src/settings/ui_schema.js
+++ web/src/settings/ui_schema.js
@@ -1,3 +1,7 @@
+import React from 'react'
+import MemoizedFontAwesomeIcon from '../util/MemoizedFontAwesomeIcon'
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
+
 export default {
   "display.display_type": {
     "ui:help": "Model of the e-paper display you're using"
@@ -25,7 +29,18 @@ export default {
   "hardware.rst_pin": {
     transformer: parseInt
   },
+  "hardware.spi_bus": {
+    "ui:help": <>
+    <div>
+      SPI bus to use.  HSPI uses GPIOs 12, 14, 15.  VSPI uses 5, 18, 19.  See README for more details.
+    </div>
+    <div>
+      <MemoizedFontAwesomeIcon icon={faExclamationTriangle} className="text-warning fa-fw mr-1" />
+      <b>Changing this setting requires a reboot!</b>
+    </div>
+    </>
+  },
   "web.port": {
     transformer: parseInt
-  }
+  },
 };
