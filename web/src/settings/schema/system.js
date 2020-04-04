883149993e159fb497fed4f5a5a560c017810833
Chris Mullins <chris@sidoh.org>
2020-04-04 15:00:44 -0700

    Add descriptive titles to timezone dropdown

--- web/src/settings/schema/system.js
+++ web/src/settings/schema/system.js
@@ -5,13 +5,22 @@ export default {
     "system.timezone": {
       $id: "#/properties/system.timezone",
       type: "string",
-      enum: [
-        "AUSET", "MSK", "CET", "UK", "UTC", "ET", "CT", "MT", "AZ", "PT"
+      oneOf: [
+        { const: "PT", title: "(GMT-07:00) Pacific Time" },
+        { const: "AZ", title: "(GMT-06:00) Arizona Time" },
+        { const: "MT", title: "(GMT-06:00) Mountain Time" },
+        { const: "CT", title: "(GMT-05:00) Central Time" },
+        { const: "ET", title: "(GMT-04:00) Eastern Time" },
+        { const: "UTC", title: "(GMT+00:00) Coordinated Universal Time" },
+        { const: "UK", title: "(GMT+00:00) Western Europe Time" },
+        { const: "CET", title: "(GMT+01:00) Central European Time" },
+        { const: "MSK", title: "(GMT+03:00) Moscow Standard Time" },
+        { const: "AUSET", title: "(GMT+10:00) Eastern Australia" },
       ],
       title: "Timezone",
       default: "",
       examples: ["PT"],
-      pattern: "^(.*)$"
-    }
-  }
+      pattern: "^(.*)$",
+    },
+  },
 };
